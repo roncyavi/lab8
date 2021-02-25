928186706660dae8a4e1121d9a98413888447238
Ramzy Oncy-Avila <roncyavi@ucsd.edu>
2021-02-25 11:04:39 -0800

    Finished lab lab 6

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'KEY';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [32.8785203,-117.2380747,17],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12,
+	  zoomControl: false
+	});
+	//Add a marker to the map
+	L.marker([32.8785203,-117.2380747,17]).addTo(map);
+
 }
